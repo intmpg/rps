649b1d52475021508fd13fd5b31fdada35923982
Georgy Biryukov <intmpg@gmail.com>
2018-10-13 16:09:11 +0300

    added code comments

--- Notebook/Notebook/Notebook.py
+++ Notebook/Notebook/Notebook.py
@@ -1,25 +1,30 @@
+#Для хранения списка контактов используется динамический список, поиск осуществляется перебором его элементов и проверкой на вхождение строки в этот элемент 
+#вывод условий
 print ('Записная книжка')
 print ('Add name - добавить контакт')
 print ('Find name - поиск контакта')
 print ('exit - выход')
 
+#создание списка контактов
 contactList = []
-command = input()
+command = input() # ввод первой команды
 
-while (command != "exit"):
+while (command != "exit"): # цикл для постоянного ввода, закончится по команде "exit"
 
-    commandArgs = command.split(' ')
+    commandArgs = command.split(' ') # разделяем команду на список двух аргументов ['команда', 'имя']
 
+    #если команда "add", то добавляем в список контактов contactList новый элемент со значением имя контакта
     if (commandArgs[0] == "Add") or (commandArgs[0] == "add"):
         contactList.append(commandArgs[1])
         print ('Добавлен контакт ', commandArgs[1])
         print (contactList)
 
+    #если же команда "find", то перебираем все элементы списка контактов и находим тот, который начинается с введенного имени
     elif (commandArgs[0] == "Find") or (commandArgs[0] == "find"):
-        count = 0
+        count = 0 #счетчик контактов, обнуляется каждый раз когда вызывается команда поиска
         for index, item in enumerate(contactList):
             if contactList[index].startswith(commandArgs[1]):
-                count = count + 1
-        print(count)
+                count = count + 1 #если строка из списка контактов начинается со строки, введенной как имя контакта, то прибавляем 1 к количеству искомых контактов
+        print(count) #вывод количества вхождений
 
-    command = input()
+    command = input() #ввод очередной команды после выполнения
